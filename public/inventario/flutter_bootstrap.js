8e4768721a213e848a3c4cf88b3fd87876fe4117
KUNGA <kunga@MacBook-Pro-de-IT-8.local>
2026-05-28 20:30:34 -0400

    deploy: web scanner optimizado — 0ms delay, EAN-13/128, cámara trasera HD

--- public/inventario/flutter_bootstrap.js
+++ public/inventario/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2194027611" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2529975140" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
